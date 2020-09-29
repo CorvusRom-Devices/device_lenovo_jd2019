4f321a5c9db42213340556adc2112ac5e3b638c7
magaryss <71859385+magaryss@users.noreply.github.com>
2020-09-28 22:43:20 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/du_jd2019.mk
+    $(LOCAL_DIR)/aosp_jd2019.mk
 
 COMMON_LUNCH_CHOICES := \
-    du_jd2019-user \
-    du_jd2019-userdebug \
-    du_jd2019-eng
+    aosp_jd2019-user \
+    aosp_jd2019-userdebug \
+    aosp_jd2019-eng
